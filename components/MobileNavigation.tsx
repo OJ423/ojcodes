c3cb1ec2044d985d518cd95854d3d243ca981104
OJ423 <osmith.dpm@gmail.com>
2024-04-16 13:35:40 +0100

    Accessibility issues fixed.

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -13,10 +13,10 @@ export default function MobileNavigation() {
   return(
     <nav className="sm:hidden items-align-center">
       <ul>
-        <Link href="/skills"><li className={`list-none inline-block me-2 ${activePage.includes('/skills') ? `text-teal-500` : null}`}><IoIosFlame aria-label="Skills page" size={25}/></li></Link>
-        <Link href="/portfolio"><li className={`list-none inline-block me-2 ${activePage.includes('/portfolio') ? `text-teal-500` : null}`}><IoIosImages aria-label="Portfolio page" size={25}/></li></Link>
-        <Link href="/about"><li className={`list-none inline-block me-2 ${activePage.includes('/about') ? `text-teal-500` : null}`}><IoIosPerson aria-label="About me page" size={25}/></li></Link>
-        <Link href="/contact"><li className={`list-none inline-block ${activePage.includes('/contact') ? `text-teal-500` : null}`}><IoIosCall aria-label="Contact me page" size={25}/></li></Link>
+        <li className={`list-none inline-block me-2 ${activePage.includes('/skills') ? `text-teal-500` : null}`}><Link href="/skills"><IoIosFlame aria-label="Skills page" size={25}/></Link></li>
+        <li className={`list-none inline-block me-2 ${activePage.includes('/portfolio') ? `text-teal-500` : null}`}><Link href="/portfolio"><IoIosImages aria-label="Portfolio page" size={25}/></Link></li>
+        <li className={`list-none inline-block me-2 ${activePage.includes('/about') ? `text-teal-500` : null}`}><Link href="/about"><IoIosPerson aria-label="About me page" size={25}/></Link></li>
+        <li className={`list-none inline-block ${activePage.includes('/contact') ? `text-teal-500` : null}`}><Link href="/contact"><IoIosCall aria-label="Contact me page" size={25}/></Link></li>
       </ul>
     </nav>
   )
--- components/hire-me-button.tsx
+++ components/hire-me-button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HireMeButton() {
   return(
     <Link href="/contact">
-      <div className="rounded-md bg-teal-500 text-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500">Hire me</div>
+      <div className="rounded-md bg-teal-500 !text-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500">Hire me</div>
     </Link>
   )
 }
